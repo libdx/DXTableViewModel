c19f78c918f8b1d0e511dd548d2f12d7c6cc41b5
Alexander Ignatenko <avi.cpp@gmail.com>
2013-10-12 20:14:17 +0300

    Add shell script to generate appledoc documentation

---
+++ generate-doc.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+appledoc \
+    --project-name "DXTableViewModel" \
+    --project-company "DXTableViewModel" \
+    --company-id dxtableviewmodel \
+    --output /tmp \
+	DXTableViewModel
